f882b41f0efb166857acf93a786d8f6494a51819
NovaPride <novapride27@gmail.com>
2024-06-21 15:12:03 +0300

    add: search params placeholder

---
+++ src/assets/img/mana/mana.js
@@ -0,0 +1,7 @@
+import Black_Mana from "./Black_Mana.png";
+import Blue_Mana from "./Blue_Mana.png";
+import Green_Mana from "./Green_Mana.png";
+import Red_Mana from "./Red_Mana.png";
+import White_Mana from "./White_Mana.png";
+
+export default { Black_Mana, Blue_Mana, Green_Mana, Red_Mana, White_Mana };
--- src/pages/search-page/search-page.jsx
+++ src/pages/search-page/search-page.jsx
@@ -5,6 +5,7 @@ import { useDebounce } from "use-debounce";
 
 import { useMTGService } from "../../services/MTGService";
 
+import mana from "../../assets/img/mana/mana";
 import "./search-page.scss";
 
 const SearchPage = () => {
@@ -28,50 +29,169 @@ const SearchForm = () => {
   const { loading, error, getCardByName, clearError } = useMTGService();
 
   const onSubmit = async (data) => {
-    const temp = await getCardByName(data.cardName);
-    console.log(temp);
+    // const temp = await getCardByName(data.cardName);
+    // console.log(cards);
+    console.log(data);
+    //сюда как то получить строку для запроса к апи и перейти на страницу где вывядятся все подходящие
     // navigate("/казахстан");
   };
 
   const elasticSearch = async (text) => {
-    getCardByName(text)
-      .then((data) => {
-        setCards(data);
-      })
-      .then(() => setIsModal(true));
-    // navigate("/казахстан");
+    getCardByName(text).then((data) => {
+      setCards(data);
+    });
   };
 
   useEffect(() => {
-    if (searchTextDebounceValue.length > 2){
-      elasticSearch(searchTextDebounceValue)
-    } else {
-      setIsModal(false)
+    if (searchTextDebounceValue.length > 2) {
+      elasticSearch(searchTextDebounceValue);
     }
-      
   }, [searchTextDebounceValue]);
 
-  const modal = isModal ? <Modal cards={cards} /> : null;
+  const modal = isModal ? (
+    <Modal cards={cards} />
+  ) : (
+    <Modal cards={cards} style={"card_modal_hide"} />
+  );
+
+  //register re-writes onBlur, onChange, value and name. so u need do to this stuff
+  const { onBlur, onChange, ...fieldProps } = register("cardName", {
+    required: true,
+  });
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="search_form">
       <div className="search_form_elem">
         <input
-          {...register("cardName", { required: true })}
           className="search_form_name"
           autoComplete="off"
           value={searchText}
-          onChange={({ target }) => setSearchText(target.value)}
+          onFocus={() => setIsModal(true)}
+          onBlur={(e) => {
+            setIsModal(false);
+            onBlur(e);
+          }}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+            onChange(e);
+          }}
+          {...fieldProps}
         />
         <input value="SEARCH" type="submit" className="search_form_submit" />
       </div>
       {modal}
+      <div className="search_form_grid">
+        <div className="search_form_grid_column">
+          <div className="search_form_grid_column_elem search_form_grid_column_elem_text">
+            Search cards by:
+          </div>
+          <div className="search_form_grid_column_elem">
+            <label htmlFor="searchType-name">
+              <input
+                {...register("searchType")}
+                type="radio"
+                value="name"
+                id="searchType-name"
+                className="search_form_grid_column_elem_radio"
+                defaultChecked={true}
+              />
+              Name
+            </label>
+            <label htmlFor="searchType-multiId">
+              <input
+                {...register("searchType")}
+                type="radio"
+                value="multiId"
+                id="searchType-multiId"
+                className="search_form_grid_column_elem_radio"
+              />
+              MultiId
+            </label>
+            <label htmlFor="searchType-smth">
+              <input
+                {...register("searchType")}
+                type="radio"
+                value="smth"
+                id="searchType-smth"
+                className="search_form_grid_column_elem_radio"
+              />
+              Smth
+            </label>
+          </div>
+          <div className="search_form_grid_column_elem search_form_grid_column_elem_separator"></div>
+          <div className="search_form_grid_column_elem search_form_grid_column_elem_text">
+            Search by Color:
+          </div>
+          <div className="search_form_grid_column_elem">
+            <label htmlFor="mana-white">
+              <input
+                {...register("mana")}
+                type="checkbox"
+                value="white"
+                id="mana-white"
+                className="search_form_grid_column_elem_checkbox"
+              />
+              <img src={mana.White_Mana} alt="White_Mana" />
+            </label>
+            <label htmlFor="mana-blue">
+              <input
+                {...register("mana")}
+                type="checkbox"
+                value="blue"
+                id="mana-blue"
+                className="search_form_grid_column_elem_checkbox"
+              />
+              <img src={mana.Blue_Mana} alt="Blue_Mana" />
+            </label>
+            <label htmlFor="mana-black">
+              <input
+                {...register("mana")}
+                type="checkbox"
+                value="black"
+                id="mana-black"
+                className="search_form_grid_column_elem_checkbox"
+              />
+              <img src={mana.Black_Mana} alt="Black_Mana" />
+            </label>
+            <label htmlFor="mana-red">
+              <input
+                {...register("mana")}
+                type="checkbox"
+                value="red"
+                id="mana-red"
+                className="search_form_grid_column_elem_checkbox"
+              />
+              <img src={mana.Red_Mana} alt="Red_Mana" />
+            </label>
+            <label htmlFor="mana-green">
+              <input
+                {...register("mana")}
+                type="checkbox"
+                value="green"
+                id="mana-green"
+                className="search_form_grid_column_elem_checkbox"
+              />
+              <img src={mana.Green_Mana} alt="Green_Mana" />
+            </label>
+          </div>
+        </div>
+        <div className="search_form_grid_column"></div>
+      </div>
     </form>
   );
 };
 
-const Modal = ({ cards }) => {
-  if (Object.keys(cards).length === 0) return <></>;
+const Modal = ({ cards, style }) => {
+  if (Object.keys(cards).length === 0)
+    return (
+      <ul className={"card_modal " + style}>
+        <li key={666} className="card_modal_elem_wrapper">
+          <div className="card_modal_elem_name">
+            <span>There is no cards with that name...</span>
+          </div>
+        </li>
+      </ul>
+    );
   const cardsSize = Object.keys(cards).length;
   const view =
     cardsSize >= 10
@@ -84,7 +204,9 @@ const Modal = ({ cards }) => {
             </li>
           ) : (
             <li key={id} className="card_modal_elem">
-              <Link to={"/card/" + multiverseid} className="card_modal_elem_wrapper">
+              <Link
+                to={"/card/" + multiverseid}
+                className="card_modal_elem_wrapper">
                 <div className="card_modal_elem_wrapper_name">{name}</div>
                 <div className="card_modal_elem_wrapper_id">{multiverseid}</div>
               </Link>
@@ -94,12 +216,14 @@ const Modal = ({ cards }) => {
       : cards.map(({ id, name, multiverseid }, i) => {
           return (
             <li key={id} className="card_modal_elem">
-              <Link to={"/card/" + multiverseid} className="card_modal_elem_wrapper">
+              <Link
+                to={"/card/" + multiverseid}
+                className="card_modal_elem_wrapper">
                 <div className="card_modal_elem_wrapper_name">{name}</div>
                 <div className="card_modal_elem_wrapper_id">{multiverseid}</div>
               </Link>
             </li>
           );
         });
-  return <ul className="card_modal">{view}</ul>;
+  return <ul className={"card_modal " + style}>{view}</ul>;
 };
