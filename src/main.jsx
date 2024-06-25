3e3e1d8a5e8585712a7a579f764913de4a338b5a
NovaPride <novapride27@gmail.com>
2024-06-25 17:49:26 +0300

    refac: try1 to vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/app/app.jsx'
-import './style/index.scss'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/app/app.jsx";
+import "./style/index.scss";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <App />
+  <App />
   // </React.StrictMode>,
-)
+);
